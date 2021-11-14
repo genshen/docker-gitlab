60de453aebd569dbd8c4c9a7973f5856a4cb1240
genshen <genshenchu@gmail.com>
2021-11-14 20:06:49 +0800

    add the missing gitlab-pages secret file

--- create.sh
+++ create.sh
@@ -140,6 +140,9 @@ ln_file ${GITLAB_CONFIG_DIR}/gitlab-shell.config.yml  ${GITLAB_SHELL_DIR}/config
 # copy gitaly config files
 ln_file ${GITLAB_CONFIG_DIR}/gitaly.config.toml  ${GITALY_DIR}/config.toml
 
+# gitlab pages secret.
+ln_file ${GITLAB_CONFIG_DIR}/pages_secret.txt  ${GITLAB_DIR}/.gitlab_pages_secret
+
 
 ## Install Init Script
 # cp ${GITLAB_DIR}/lib/support/init.d/gitlab /etc/init.d/gitlab
